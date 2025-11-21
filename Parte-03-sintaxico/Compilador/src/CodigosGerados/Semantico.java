e57159d32c316af2cc499bbe9479d6547a5febf4
Luiz Carlos Martendal <luiz.c.martendal@hotmail.com>
2025-11-20 22:34:47 -0300

    some adjustments

--- Parte-03-sintaxico/Compilador/src/CodigosGerados/Semantico.java
+++
@@ -1,9 +0,0 @@
-package CodigosGerados;
-
-public class Semantico implements Constants
-{
-    public void executeAction(int action, Token token)	throws SemanticError
-    {
-        System.out.println("Ação #"+action+", Token: "+token);
-    }	
-}
